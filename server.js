e19cfaa99ef4d15573d38e23e73f0c32c727f5f6
merchedine <merchedine@gmail.com>
2015-11-06 18:51:27 +0700

    POST for todos

--- server.js
+++ server.js
@@ -1,19 +1,12 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos =  [{
-    id:1,
-    description:'Meet mom for lunch',
-    completed:false
-},{
-    id:2,
-    description:'Kanojo no ie ni iku.',
-    completed:false
-},{
-    id:3,
-    description:'Play PSP',
-    completed:true
-}];
+var todos =  [];
+var bodyParser = require('body-parser');
+
+var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 app.get('/',function(req,res){
     res.send('Todo API Root');
@@ -35,8 +28,13 @@ app.get('/todos/:id',function(req,res){
     }
 });
 
-app.get('/todos',function(req,res){
-   res.json(todos);
+app.post('/todos',function(req,res){
+    var body = req.body;
+    body.id = todoNextId;
+    todos.push(body);
+    todoNextId++;
+    console.log('description');
+    res.json(body);
 });
 
 app.listen(PORT,function(){
