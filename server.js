b22a6ea8ddc30bcefe2bf49b0366269334ffeb9c
merchedine <merchedine@gmail.com>
2015-11-06 19:14:35 +0700

    Add sanitazaion

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var _ = require('underscore');
 var PORT = process.env.PORT || 3000;
 var todos =  [];
 var bodyParser = require('body-parser');
@@ -14,13 +15,7 @@ app.get('/',function(req,res){
 
 app.get('/todos/:id',function(req,res){
     var todoId = parseInt(req.params.id);
-    var matchesTodo;
-
-    var todo = todos.forEach(function(todo){
-        if(todo.id === todoId){
-            matchesTodo = todo;
-        }
-    });
+    var matchesTodo = _.findWhere(todos,{id:todoId});
     if(matchesTodo === undefined){
         res.status(404).send();
     } else {
@@ -30,6 +25,14 @@ app.get('/todos/:id',function(req,res){
 
 app.post('/todos',function(req,res){
     var body = req.body;
+    var body = _.pick(req.body,'description','completed');
+    console.log(!_.isBoolean(body.completed));
+    console.log(!_.isString(body.description));
+
+    if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
+       return res.status(404).send();
+    }
+    body.description = body.description.trim();
     body.id = todoNextId;
     todos.push(body);
     todoNextId++;
