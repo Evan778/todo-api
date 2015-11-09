a127e20a3add59d6326d0f5bdaa425c16bd8f0ff
merchedine <merchedine@gmail.com>
2015-11-09 11:13:42 +0700

    OKAY

--- server.js
+++ server.js
@@ -125,17 +125,21 @@ app.post('/todos',function(req,res){
 app.delete('/todo/:id',function(req,res){
     var todoId = parseInt(req.params.id,10);
 
-    var matchedTodo = _.findWhere(todos,{
-        id:todoId
+    db.todo.destroy({
+        where:{
+            id:todoId
+        }
+    }).then(function(rowsDeleted){
+        if(rowsDeleted === 0){
+            res.status(404).json({
+                error:'No todo with id'
+            });
+        } else {
+            res.status(204).send();
+        }
+    },function(){
+        res.status(500).send();
     });
-
-    if(!matchedTodo){
-        res.status(404).send();
-    } else {
-
-        todos = _.without(todos,matchedTodo);
-        res.json(matchedTodo);
-    }
 
 
 });
