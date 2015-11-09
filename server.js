87051d503bfa4300ba5783ff410dfa34d50b9a8f
merchedine <merchedine@gmail.com>
2015-11-09 11:33:57 +0700

    add user model and post/users request

--- server.js
+++ server.js
@@ -73,7 +73,6 @@ app.get('/todos',function(req,res){
 
 app.put('/todos/:id', function (req, res) {
     var todoId = parseInt(req.params.id, 10);
-    var matchedTodo = _.findWhere(todos, {id: todoId});
     var body = _.pick(req.body, 'description', 'completed');
     var validAttributes = {};
 
@@ -83,18 +82,30 @@ app.put('/todos/:id', function (req, res) {
 
     if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
         validAttributes.completed = body.completed;
-    } else if (body.hasOwnProperty('completed')) {
-        return res.status(400).send();
     }
 
     if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
         validAttributes.description = body.description;
-    } else if (body.hasOwnProperty('description')) {
-        return res.status(400).send();
     }
 
-    _.extend(matchedTodo, validAttributes);
-    res.json(matchedTodo);
+    db.todo.findById(todoId).then(function(todo) {
+        if (todo) {
+            todo.update(attributes);
+        } else{
+            res.status(404).send();
+        }
+    },function(){
+        res.status(500).send();
+    });
+});
+
+app.post('/users',function(req,res){
+    var body = _.pick(req.body,'email','password');
+    db.user.create(body).then(function(user){
+       res.json(user.toJSON());
+    }, function(e){
+        res.status(400).json(e);
+    });
 });
 
 app.post('/todos',function(req,res){
@@ -108,18 +119,7 @@ app.post('/todos',function(req,res){
         res.status(400).json(e);
     });
 
-    //console.log(!_.isBoolean(body.completed));
-    //console.log(!_.isString(body.description));
-    //
-    //if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
-    //   return res.status(404).send();
-    //}
-    //body.description = body.description.trim();
-    //body.id = todoNextId;
-    //todos.push(body);
-    //todoNextId++;
-    //console.log('description');
-    //res.json(body);
+
 });
 
 app.delete('/todo/:id',function(req,res){
